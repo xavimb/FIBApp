56812ac40748c117d9bea45ab822b1b2d4b85a73
Xavi Magrinyà <xavi.mb89@gmail.com>
2013-04-29 15:33:43 +0200

    Initial Commit

---
+++ FIBApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FIBApp
+//
+//  Created by Xavi Magrinyà on 4/29/13.
+//  Copyright (c) 2013 Xavi Magrinyà. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FIBApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FIBApp
+//
+//  Created by Xavi Magrinyà on 4/29/13.
+//  Copyright (c) 2013 Xavi Magrinyà. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
